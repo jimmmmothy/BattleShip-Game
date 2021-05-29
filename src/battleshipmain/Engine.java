30bcee8eedc7f7d83e16b2f217cdb0702370b343
MishoMish <ari_2014@mail.bg>
2021-05-29 16:07:16 +0300

    Added MouseMotionListener

--- src/battleshipmain/Engine.java
+++ src/battleshipmain/Engine.java
@@ -1,19 +1,39 @@
 package battleshipmain;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
 public class Engine {
-    public void placeShips(){
-        
-    }
-    
-    public void playerTurn(){
-        
-    }
-    
-    public void enemyTurn(){
-        
-    }
-    
-    private void findMouseCoords(){
-        
-    }
+	public void placeShips() {
+
+	}
+
+	public void playerTurn() {
+
+	}
+
+	public void enemyTurn() {
+
+	}
+
+	private void findMouseCoords() {
+
+	}
+
+	public static void main(String[] args) {
+		Board board = new Board();
+		board.playerOcean.addMouseMotionListener(new MouseMotionListener() {
+
+			@Override
+			public void mouseMoved(MouseEvent e) {
+				System.out.println("1    " + (e.getX()-30));
+			}
+
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				System.out.println("2    " + (e.getX()-30));
+			}
+		});
+	}
 }
