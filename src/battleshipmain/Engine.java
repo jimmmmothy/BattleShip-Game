53123f87d7e2746839591bcb036e64e4084eaa7f
Dimitar Atanasov <sencor0442@gmail.com>
2021-05-29 17:01:40 +0300

    Added highlight functionality to both oceans

--- src/battleshipmain/Engine.java
+++ src/battleshipmain/Engine.java
@@ -3,31 +3,38 @@ package battleshipmain;
 import java.awt.event.*;
 
 public class Engine {
+
     public Board board;
     private int x;
     private int y;
-    
-    public Engine(Board board){
+
+    public Engine(Board board) {
         this.board = board;
     }
-    
-    public void run(){
+
+    public void run() {
         board.playerOcean.addMouseMotionListener(new MouseMotionListener() {
 
             @Override
             public void mouseMoved(MouseEvent e) {
                 x = e.getX() - board.playerOcean.padding;
                 y = e.getY() - board.playerOcean.padding;
-                
+
                 System.out.println(x + " " + y);
-                
-                if (x >= 0 &&
-                    x < board.playerOcean.size * board.playerOcean.cellCount &&
-                    y >= 0 &&
-                    y < board.playerOcean.size * board.playerOcean.cellCount) {
+
+                if (x >= 0
+                        && x < board.playerOcean.size * board.playerOcean.cellCount
+                        && y >= 0
+                        && y < board.playerOcean.size * board.playerOcean.cellCount) {
                     highlightCellFromCoords(board.playerOcean);
+                } else {
+                    for (int i = 0; i < board.playerOcean.cellCount; i++) {
+                        for (int j = 0; j < board.playerOcean.cellCount; j++) {
+                            board.playerOcean.cells[i][j].unHighlight();
+                        }
+                    }
                 }
-                
+
                 board.playerOcean.repaint();
             }
 
@@ -36,35 +43,93 @@ public class Engine {
 
             }
         });
-        
-        board.playerOcean.addMouseListener(new MouseListener(){
+
+        board.playerOcean.addMouseListener(new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent me) {
-                
+
             }
 
             @Override
             public void mousePressed(MouseEvent me) {
-                
+
             }
 
             @Override
             public void mouseReleased(MouseEvent me) {
-                
+
             }
 
             @Override
             public void mouseEntered(MouseEvent me) {
-                
+
             }
 
             @Override
             public void mouseExited(MouseEvent me) {
-                
+
+            }
+        });
+
+        board.enemyOcean.addMouseMotionListener(new MouseMotionListener() {
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                x = e.getX() - board.enemyOcean.padding;
+                y = e.getY() - board.enemyOcean.padding;
+
+                System.out.println(x + " " + y);
+
+                if (x >= 0
+                        && x < board.enemyOcean.size * board.enemyOcean.cellCount
+                        && y >= 0
+                        && y < board.enemyOcean.size * board.enemyOcean.cellCount) {
+                    highlightCellFromCoords(board.enemyOcean);
+                } else {
+                    for (int i = 0; i < board.enemyOcean.cellCount; i++) {
+                        for (int j = 0; j < board.enemyOcean.cellCount; j++) {
+                            board.enemyOcean.cells[i][j].unHighlight();
+                        }
+                    }
+                }
+
+                board.enemyOcean.repaint();
+            }
+
+            @Override
+            public void mouseDragged(MouseEvent e) {
+
+            }
+        });
+
+        board.enemyOcean.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent me) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent me) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent me) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent me) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent me) {
+
             }
         });
     }
-    
+
     private void placeShips() {
 
     }
@@ -76,17 +141,17 @@ public class Engine {
     private void enemyTurn() {
 
     }
-    
-    private void highlightCellFromCoords(Ocean ocean){
+
+    private void highlightCellFromCoords(Ocean ocean) {
         int currX = x / ocean.size;
         int currY = y / ocean.size;
-        
+
         for (int i = 0; i < ocean.cellCount; i++) {
             for (int j = 0; j < ocean.cellCount; j++) {
                 ocean.cells[i][j].unHighlight();
             }
         }
-        
+
         ocean.cells[currX][currY].highlight();
     }
 }
